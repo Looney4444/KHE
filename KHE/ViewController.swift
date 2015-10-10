8999c8535f8defbc4ea2b2e42ccac05aec7a98d4
Albert Looney <alooney@capital.edu>
2015-10-09 21:59:41 -0400

    init commit

---
+++ KHE/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KHE
+//
+//  Created by Albert Looney on 10/9/15.
+//  Copyright © 2015 Albert Looney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
